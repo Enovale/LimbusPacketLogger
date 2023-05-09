eab0448e0be19586d357fabe04a3673c0d3051ae
Enova <17408285+Enovale@users.noreply.github.com>
2023-05-09 17:37:24 -0500

    Initial Commit

---
+++ PacketLogger/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace PacketLogger
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BasePlugin
+    {
+        public static ManualLogSource PluginLog;
+
+        public static ConfigEntry<bool> LogPackets;
+
+        public Plugin()
+        {
+            PluginLog = Log;
+        }
+
+        public override void Load()
+        {
+            // Plugin startup logic
+            var harmony = new Harmony($"com.enovale.{MyPluginInfo.PLUGIN_GUID}");
+            harmony.PatchAll(typeof(RequesterPatches));
+
+            LogPackets = Config.Bind("General", nameof(LogPackets), true,
+                "Whether or not to print outgoing packets to the console.");
+            
+            Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
---
+++ PacketLogger/RequesterPatches.cs
@@ -0,0 +1,16 @@
+using HarmonyLib;
+using Server;
+
+namespace PacketLogger
+{
+    public static class RequesterPatches
+    {
+        [HarmonyPatch(typeof(HttpApiRequester), nameof(HttpApiRequester.AddRequest))]
+        [HarmonyPrefix]
+        public static void PacketWrapper(ref HttpApiSchema __0)
+        {
+            if (Plugin.LogPackets.Value)
+                Plugin.PluginLog.LogWarning($"Command Sent: {__0.RequestJson}");
+        }
+    }
+}
